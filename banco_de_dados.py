e7e5ccdb522251243a8aede781a910d149b3275b
CristianoVerissimo <cristiano.verissimo87@gmail.com>
2023-09-06 17:22:04 -0300

    Commit 1

---
+++ banco_de_dados.py
@@ -0,0 +1,65 @@
+# Banco de dados SQLITE3
+import sqlite3
+
+# Conexão com banco de dados
+try:
+    con = sqlite3.connect('cadastro_alunos.db')
+    print('Conexão com banco de dados realizada com sucesso!')
+except sqlite3.Error as e:
+    print("Erro ao conectar com banco de dados", e)
+
+# Tabela de Cursos
+try:
+    with con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS cursos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            duracao TEXT,
+            preco REAL
+        )""")
+        
+        print("Tabela de cursos criada com sucesso!")
+        
+except sqlite3.Error as e:
+    print("Erro ao criar tabela de cursos", e)
+
+# Tabela de Turmas
+try:
+    with con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS turmas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            nome_cursos TEXT,
+            data_inicio DATE,
+            FOREIGN KEY (nome_cursos) REFERENCES cursos (nome) ON UPDATE CASCADE ON DELETE CASCADE
+        )""")
+        
+        print("Tabela de turmas criada com sucesso!")
+        
+except sqlite3.Error as e:
+    print("Erro ao criar tabela de turmas", e)
+
+# Tabela de Alunos
+try:
+    with con:
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS alunos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            data_nascimento DATE,
+            telefone TEXT
+            endereco TEXT,
+            email TEXT,
+            sexo TEXT,
+            foto TEXT,
+            cpf TEXT,
+            nome_turma TEXT,
+            FOREIGN KEY (nome_turma) REFERENCES turmas (nome) ON DELETE CASCADE
+        )""")
+        
+        print("Tabela de alunos criada com sucesso!")
+        
+except sqlite3.Error as e:
+    print("Erro ao criar tabela de alunos", e)
---
+++ view.py
@@ -0,0 +1,52 @@
+# Banco de dados SQLITE3 lite
+import sqlite3 as lite
+
+# Conexão com banco de dados
+try:
+    con = lite.connect('cadastro_alunos.db')
+    print('Conexão com banco de dados realizada com sucesso!')
+except lite.Error as e:
+    print("Erro ao conectar com banco de dados", e)
+    
+# Tabela de Cursos
+#Criar Cursos
+def criar_cursos(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO Cursos (nome, duracao, preco) VALUES (?,?,?)"
+        cur.execute(query,i)
+        
+#criar_cursos(['Violão','Semanas','10'])
+
+# Listar Cursos
+def listar_cursos():
+    lista = []
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM cursos')
+        linha = cur.fetchall()
+        
+        for i in linha:
+            lista.append(i)
+    return lista
+
+#print(listar_cursos())
+
+#Atualizar Cursos
+def atualizar_cursos(i):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE cursos SET nome=?, duracao=?, preco=? WHERE id=?"
+        cur.execute(query,i)
+
+l = ['Violão', '2 Semanas', 10.0, 1]        
+#atualizar_cursos(l)
+
+#Deletar Cursos
+def deletar_cursos(i):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM cursos WHERE id=?"
+        cur.execute(query,i)
+        
+deletar_cursos([1])
