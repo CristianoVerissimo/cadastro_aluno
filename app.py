46d54e094264d171bec17479852513fe6b128ee1
CristianoVerissimo <cristiano.verissimo87@gmail.com>
2023-09-14 14:20:27 -0300

    Mudança de IP e Porta

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ async def index():
         
     return data
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='0.0.0.0', port=7777)
+    uvicorn.run(app=app, host='127.0.0.1', port=8000)
